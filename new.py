031edb0f06e68cec13f7362da8f3dc32581e4ca2
Shea Greene <sheagreene@Admins-Mac-mini.local>
2024-09-07 12:55:59 +0100

    feat: new changes

--- new.py
+++ new.py
@@ -1,4 +1,7 @@
 days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
 for day in days:
-  print("day:")
   print(day)
+  if day == 'monday':
+    print("1")
+    if day == 'tuesday':
+      print("2")
