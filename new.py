fe6994bab2fe43a3d4f67ae717a70f765ecd2e6b
Itsmebob1 <152779334+Itsmebob1@users.noreply.github.com>
2024-09-13 17:56:43 +0100

    Update new.py

--- new.py
+++ new.py
@@ -2,6 +2,16 @@ days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sun
 for day in days:
   print(day)
   if day == 'monday':
-    print("1")
-    if day == 'tuesday':
+      print("1")
+  if day == 'tuesday':
       print("2")
+  if day == 'wednesday':
+      print("3")
+  if day == 'thursday':
+      print("4")
+  if day == 'friday':
+      print("5")
+  if day == 'saturday':
+      print("6")
+  if day == 'sunday':
+      print("7")
